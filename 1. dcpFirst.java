00e88fdf1824cb3c1275cd67195ec0068ec474cf
Arush Kamboj <arushkamboj@gmail.com>
2018-11-11 17:10:43 -0800

    DCP: Problem #1

--- 1. dcpFirst.java
+++ 1. dcpFirst.java
@@ -1,7 +1,7 @@
 import java.util.HashSet;
 import java.util.Set;
 
-/* *
+/* * *
 * This problem was recently asked by Google.
 
 * Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
